33991c14530d58049f8b0918c84ee7440e142107
Jayson Lennon <jayson@jaysonlennon.dev>
2020-04-21 05:20:36 -0700

    Modify stack program

--- stack/Program.cs
+++ stack/Program.cs
@@ -2,11 +2,49 @@
 
 namespace stack
 {
+    public class Stack
+    {
+        Entry top;
+        public void Push(object data)
+        {
+            top = new Entry(top, data);
+        }
+
+        public object Pop()
+        {
+            if (top == null)
+            {
+                throw new InvalidOperationException();
+            }
+            object result = top.data;
+            top = top.next;
+            return result;
+        }
+
+        class Entry
+        {
+            public Entry next;
+            public object data;
+            public Entry(Entry next, object data)
+            {
+                this.next = next;
+                this.data = data;
+            }
+        }
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Stack stack = new Stack();
+            stack.Push("world");
+            stack.Push("hello");
+
+            // hello
+            Console.WriteLine(stack.Pop());
+            // world
+            Console.WriteLine(stack.Pop());
         }
     }
 }
