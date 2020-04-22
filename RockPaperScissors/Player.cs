df93e36f67712e375526b44f99e2014f7ba053f4
Jayson Lennon <jayson@jaysonlennon.dev>
2020-04-22 16:46:23 -0700

    Use consistent braces

--- RockPaperScissors/Player.cs
+++ RockPaperScissors/Player.cs
@@ -1,16 +1,20 @@
 using System;
 
-namespace RockPaperScissors {
-    class Player {
+namespace RockPaperScissors
+{
+    class Player
+    {
         public string name;
         public int wins, losses;
 
-        public Player(string name) {
+        public Player(string name)
+        {
             this.name = name;
         }
 
-        public static Player FromConsole(string prompt) {
-			Console.Write(prompt);
+        public static Player FromConsole(string prompt)
+        {
+            Console.Write(prompt);
             return new Player(Console.ReadLine());
         }
     }
--- RockPaperScissors/Program.cs
+++ RockPaperScissors/Program.cs
@@ -3,11 +3,12 @@ using System.Collections.Generic;
 
 namespace RockPaperScissors
 {
-	class Program {
-		static void Main(string[] args)
-		{
-			Game game = new Game();
-			game.Run();
-		}
-	}
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Game game = new Game();
+            game.Run();
+        }
+    }
 }
