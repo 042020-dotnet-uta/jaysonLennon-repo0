485bb2325f25abc29393adf20bee13bb1c5cfd69
Jayson Lennon <jayson@jaysonlennon.dev>
2020-05-01 18:09:26 -0700

    Add Login name column

---
+++ Project0/StoreDb/Migrations/20200502010830_UserLogin.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace StoreDb.Migrations
+{
+    public partial class UserLogin : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("ALTER TABLE Customers ADD COLUMN Login TEXT");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
