e069f1598cc479b3ddc885fcc00f55a1f22eeb41
Jayson Lennon <jayson@jaysonlennon.dev>
2020-05-05 08:18:06 -0700

    Can now order multiple items without leaving menu

--- Project0/StoreCli/Menus/User/Program/OrderItems.cs
+++ Project0/StoreCli/Menus/User/Program/OrderItems.cs
@@ -7,11 +7,14 @@ using StoreDb;
 
 namespace StoreCliMenuUser
 {
+
     /// <summary>
     /// Menu to place an order.
     /// </summary>
     class OrderItems : CliMenu, IMenu
     {
+        private List<Guid> InventoryIds = new List<Guid>();
+
         /// <summary>
         /// Create this menu.
         /// </summary>
@@ -42,6 +45,21 @@ namespace StoreCliMenuUser
         {
             Console.Clear();
             CliPrinter.Title("Order Items");
+            using (var db = new StoreContext(this.ApplicationState.DbOptions))
+            {
+                var inventory = db.GetProductsAvailable(this.ApplicationState.UserData.OperatingLocationId);
+                inventory.Where(i => i.Quantity > 0);
+
+                var i = 1;
+                this.InventoryIds = inventory.Select(i => i.LocationInventoryId).ToList();
+                Console.WriteLine("#\tStock\tName");
+                foreach (var item in inventory)
+                {
+                    Console.WriteLine($"{i}.\t{item.Quantity}\t{item.Product.Name}");
+                    i += 1;
+                }
+                Console.Write("\n----------------------------------------------\n");
+            }
         }
 
         /// <summary>
@@ -49,62 +67,46 @@ namespace StoreCliMenuUser
         /// </summary>
         public void InputLoop()
         {
-            using (var db = new StoreContext(this.ApplicationState.DbOptions))
+            do
             {
-                var inventory = db.GetProductsAvailable(this.ApplicationState.UserData.OperatingLocationId);
-                inventory.Where(i => i.Quantity > 0);
-
-                var i = 1;
-                List<Guid> inventoryIds = inventory.Select(i => i.LocationInventoryId).ToList();
-                Console.WriteLine("#\tStock\tName");
-                foreach (var item in inventory)
+                PrintMenu();
+                using (var db = new StoreContext(this.ApplicationState.DbOptions))
                 {
-                    Console.WriteLine($"{i}.\t{item.Quantity}\t{item.Product.Name}");
-                    i += 1;
-                }
 
-                bool itemNumberValidator(int num) {
-                    if (num > 0 && num <= i) return true;
-                    else {
-                        CliPrinter.Error($"Please enter an inventory number between 1 and {i}");
-                        return false;
+                    bool itemNumberValidator(int num) {
+                        if (num > 0 && num <= this.InventoryIds.Count) return true;
+                        else {
+                            CliPrinter.Error($"Please enter an inventory number between 1 and {this.InventoryIds.Count}");
+                            return false;
+                        }
                     }
-                }
 
-                Console.Write("\n----------------------------------------------\n");
+                    var itemIndex = CliInput.GetInt(CliInput.GetIntOptions.AllowEmpty, itemNumberValidator, "Inventory number: ") ?? 0;
+                    if (itemIndex == 0) break;
 
-                var itemIndex = CliInput.GetInt(CliInput.GetIntOptions.AllowEmpty, itemNumberValidator, "Inventory number: ") ?? 0;
-                if (itemIndex == 0) {
-                    this.AbortThenExit("No item selected - exiting.");
-                    return;
-                }
+                    var inventoryId = this.InventoryIds[itemIndex - 1];
+                    var maxQuantity = db.GetInventory(inventoryId).Quantity;
 
-                var inventoryId = inventoryIds[itemIndex - 1];
-                var maxQuantity = db.GetInventory(inventoryId).Quantity;
-
-                bool quantityValidator(int num) {
-                    if (num >= 0 && num <= maxQuantity) return true;
-                    else {
-                        CliPrinter.Error($"Please enter a quantity between 0 and {maxQuantity}");
-                        return false;
+                    bool quantityValidator(int num) {
+                        if (num >= 0 && num <= maxQuantity) return true;
+                        else {
+                            CliPrinter.Error($"Please enter a quantity between 0 and {maxQuantity}");
+                            return false;
+                        }
                     }
+
+                    var orderQuantity = CliInput.GetInt(CliInput.GetIntOptions.AllowEmpty, quantityValidator, "Quantity: ") ?? 0;
+                    if (orderQuantity == 0) continue;
+
+                    var product = db.GetProductFromInventoryId(inventoryId);
+                    var order = db.FindCurrentOrder(this.ApplicationState.UserData.CustomerId);
+                    db.AddLineItem(order, product, orderQuantity);
+                    db.SaveChanges();
+
+                    CliInput.PressAnyKey("\nAdded to order.");
                 }
-
-                var orderQuantity = CliInput.GetInt(CliInput.GetIntOptions.AllowEmpty, quantityValidator, "Quantity: ") ?? 0;
-                if (orderQuantity == 0)
-                {
-                    this.AbortThenExit("No quantity selected - exiting.");
-                    return;
-                }
-
-                var product = db.GetProductFromInventoryId(inventoryId);
-                var order = db.FindCurrentOrder(this.ApplicationState.UserData.CustomerId);
-                db.AddLineItem(order, product, orderQuantity);
-                db.SaveChanges();
-
-                CliInput.PressAnyKey("\nAdded to order.");
-                this.MenuExit();
-            }
+            } while (true);
+            this.MenuExit();
         }
     }
 }
